2b6df4ea106db6ca9d97bf98467430d1d572e50a
Mohammad Shadab Hussain <shadab.iiitnr20@gmail.com>
2024-06-22 19:25:04 +0530

    Testing

--- app1/src/pages/Page1.tsx
+++ app1/src/pages/Page1.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Example from 'exampleApp/example';
 
 export function Page1() {
   return (
     <React.Fragment>
       <div>Page 1 from App1</div>
-      <Link to="/page-2">Go to Page 2</Link>
+      <Link to='/page-2'>Go to Page 2</Link>
     </React.Fragment>
   );
 }
--- shell/src/App.tsx
+++ shell/src/App.tsx
@@ -1,7 +1,11 @@
-import React from "react";
-import "./index.css";
-import { Router } from "./routing/Router";
+import React from 'react';
+import './index.css';
+import { Router } from './routing/Router';
+import Example from './components/Example';
 
 export const App = () => (
-  <Router />
+  <>
+    <Router />
+    <Example />
+  </>
 );
